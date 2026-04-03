d12c722db8858e27e0fa6f2d1cac81430816989c
tejas-muthya38 <tejas.muthya38@gmail.com>
2026-04-03 12:50:41 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        if (nums.every(num => num === 0)) return nums;
+        if (nums.filter(num => num === 0).length > 1) return nums.fill(0);
+        let product = 1;
+        let productWithoutZero = 1;
+        let output = [];
+        for (let i=0;i<nums.length;i++) {
+            product = product * nums[i];
+            if (nums[i] !== 0) productWithoutZero = productWithoutZero * nums[i];
+        }
+        for (let i=0;i<nums.length;i++) {
+            if (nums[i] === 0) output[i] = productWithoutZero;
+            else output[i] = product / nums[i];
+        }
+        return output;
+    }
+}
